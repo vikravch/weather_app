f637a5737a401567accbbb21ec4bece18d347afe
vitaliikravchyk <vitaly.kravchyk@gmail.com>
2024-03-11 19:28:17 +0200

    Webinar 42 added use effect

--- src/modules/weather/presentation/pages/UserListWeatherPage.jsx
+++ src/modules/weather/presentation/pages/UserListWeatherPage.jsx
@@ -1,14 +1,16 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect} from 'react';
 import {AppContext} from "../../../../general/context/appContext";
 
 function UserListWeatherPage() {
     const {prefsCities, initPrefsCities} = useContext(AppContext);
+
+    useEffect(() => {
+        initPrefsCities()
+    }, []);
+
     return (
         <>
             <h1>User List Weather Page</h1>
-            <button onClick={()=>{
-                initPrefsCities()
-            }}>Init cities</button>
             <ul>
                 {prefsCities.map((item, i) => {
                     return <li key={i.toString()}>{item}</li>
